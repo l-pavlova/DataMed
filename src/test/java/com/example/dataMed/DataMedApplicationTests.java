1ee209afe4a726c2111c7e22bb7f5ffec6e629a2
vasilnv <vasil.vasilev1999@abv.bg>
2022-01-08 21:09:25 +0200

    init commit

---
+++ src/test/java/com/example/dataMed/DataMedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dataMed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
