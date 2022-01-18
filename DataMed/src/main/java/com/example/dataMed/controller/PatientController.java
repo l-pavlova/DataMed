0a087bcba3af4e579be19e2f3903bd54bc66000a
vasilnv <vasil.vasilev1999@abv.bg>
2022-01-18 23:04:21 +0200

    minor

--- DataMed/src/main/java/com/example/dataMed/controller/PatientController.java
+++ DataMed/src/main/java/com/example/dataMed/controller/PatientController.java
@@ -6,12 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.dataMed.controller.mappper.PatientModelMapper;
 import com.example.dataMed.dto.PatientDto;
