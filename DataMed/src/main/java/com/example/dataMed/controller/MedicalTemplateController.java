92ba07241880507b6c032ffc4cbd35a6d852c5af
Zehra Iliyaz <zehrash11@gmail.com>
2022-01-22 14:33:55 +0200

    implement functionality for fetching and adding templates

---
+++ DataMed/src/main/java/com/example/dataMed/controller/MedicalTemplateController.java
@@ -0,0 +1,42 @@
+package com.example.dataMed.controller;
+
+import com.example.dataMed.dto.MedicalTemplateDto;
+import com.example.dataMed.model.MedicalTemplate;
+import com.example.dataMed.service.MedicalTemplateService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+@RequestMapping(value = "/template")
+@RestController
+public class MedicalTemplateController {
+    private ModelMapper modelMapper = new ModelMapper();
+
+    @Autowired
+    private MedicalTemplateService medicalTemplateService;
+
+    @GetMapping("/templates")
+    public ResponseEntity getAllTemplates() throws IOException {
+        List<MedicalTemplate> medicalTemplates = medicalTemplateService.getAllTemplates();
+        List<MedicalTemplateDto> medicalTemplatesDto = Arrays.asList(modelMapper.map(medicalTemplates, MedicalTemplateDto[].class));
+        return new ResponseEntity(medicalTemplatesDto, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/template")
+    public ResponseEntity<MedicalTemplateDto> getTemplate(@RequestParam("file") String filename) {
+        MedicalTemplate medicalTemplate = medicalTemplateService.getTemplate(filename);
+        return new ResponseEntity<>(modelMapper.map(medicalTemplate,MedicalTemplateDto.class), HttpStatus.OK);
+    }
+
+    @PostMapping("/uploadTemplate")
+    public ResponseEntity addTemplate(@RequestParam("file") MultipartFile file) {
+        return medicalTemplateService.addTemplate(file);
+    }
+}
