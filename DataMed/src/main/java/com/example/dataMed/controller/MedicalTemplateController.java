69dbb8f2ede47a63e6cf1853fc83f1b6997b3d5b
asus <asus@LAPTOP-838BP660>
2022-01-22 16:44:25 +0200

    Fix annotation

--- DataMed/src/main/java/com/example/dataMed/controller/MedicalTemplateController.java
+++ DataMed/src/main/java/com/example/dataMed/controller/MedicalTemplateController.java
@@ -4,13 +4,12 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,7 +36,7 @@ public class MedicalTemplateController {
     }
 
     @GetMapping("/{file}")
-    public ResponseEntity<MedicalTemplateDto> getTemplate(@PathParam("file") String filename) {
+    public ResponseEntity<MedicalTemplateDto> getTemplate(@PathVariable("file") String filename) {
         MedicalTemplate medicalTemplate = medicalTemplateService.getTemplate(filename);
         return new ResponseEntity<>(modelMapper.map(medicalTemplate, MedicalTemplateDto.class), HttpStatus.OK);
     }
