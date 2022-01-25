009f36ee84d0d3d01d6da82f5ac3cff9063bbcce
asus <asus@LAPTOP-838BP660>
2022-01-25 13:30:15 +0200

    Add role to login response

--- DataMed/src/main/java/com/example/dataMed/authentication/DataMedAuthenticationSuccessHandler.java
+++ DataMed/src/main/java/com/example/dataMed/authentication/DataMedAuthenticationSuccessHandler.java
@@ -7,9 +7,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.TextNode;
 
 public class DataMedAuthenticationSuccessHandler implements AuthenticationSuccessHandler{
 
@@ -17,8 +22,15 @@ public class DataMedAuthenticationSuccessHandler implements AuthenticationSucces
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
 		response.setStatus(HttpStatus.OK.value());
+		
+		ObjectMapper mapper = new ObjectMapper();
+		ObjectNode principal = mapper.createObjectNode();
+		principal.set("id", new TextNode(authentication.getName()));
+		principal.set("role", new TextNode(authentication.getAuthorities().iterator().next().getAuthority()));
+		
+		response.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
 		PrintWriter writer = response.getWriter();
-		writer.write(authentication.getName());
+		writer.write(principal.toString());
 		writer.flush();
 		writer.close();
 	}
