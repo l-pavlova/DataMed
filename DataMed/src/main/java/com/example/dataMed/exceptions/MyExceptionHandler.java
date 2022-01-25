49ba90ec6266c8a925d2436288aaa79a80ed3522
vasilnv <vasil.vasilev1999@abv.bg>
2022-01-25 22:20:11 +0200

    return json

--- DataMed/src/main/java/com/example/dataMed/exceptions/MyExceptionHandler.java
+++ DataMed/src/main/java/com/example/dataMed/exceptions/MyExceptionHandler.java
@@ -1,6 +1,11 @@
 package com.example.dataMed.exceptions;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+import org.apache.catalina.connector.Response;
 import org.hibernate.PropertyValueException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,6 +13,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 import java.sql.SQLIntegrityConstraintViolationException;
 
 @ControllerAdvice
@@ -15,9 +22,14 @@ public class MyExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {SQLIntegrityConstraintViolationException.class})
     protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
-        return new ResponseEntity<Object>("Entity already exists", HttpStatus.CONFLICT);
+        String message = "Entity already exists";;
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode principal = mapper.createObjectNode();
+        principal.set("message", new TextNode(message));
+        return ResponseEntity.status(HttpStatus.CONFLICT).header(HttpHeaders.CONTENT_TYPE, "application/json").body(principal);
     }
 
+    // this is never hit, leave just in case
     @ExceptionHandler(value = {PropertyValueException.class})
     protected ResponseEntity<Object> handleBadRequest(RuntimeException ex, WebRequest request) {
         return new ResponseEntity<Object>("Missing password", HttpStatus.BAD_REQUEST);
