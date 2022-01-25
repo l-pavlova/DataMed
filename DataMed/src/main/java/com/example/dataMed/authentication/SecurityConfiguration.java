d7aacce6fcd1a8045d903e169cdd93deb613fa99
asus <asus@LAPTOP-838BP660>
2022-01-25 14:24:39 +0200

    Add logout success handler

--- DataMed/src/main/java/com/example/dataMed/authentication/SecurityConfiguration.java
+++ DataMed/src/main/java/com/example/dataMed/authentication/SecurityConfiguration.java
@@ -6,11 +6,13 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -36,7 +38,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		.anyRequest().authenticated()
 		.and()
 		.logout().invalidateHttpSession(true)
-		.logoutUrl("/logout");
+		.logoutUrl("/logout")
+		.logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK));
 	}
 	
 	@Override
